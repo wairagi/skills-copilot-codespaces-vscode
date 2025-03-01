b5486c5109204b4126339c4c3157a7c475085ee7
vkuCoding <vineeth.k.upadhya@intel.com>
2025-03-01 17:51:33 +0530

    Copilot 3rd Commit

---
+++ comment.js
@@ -0,0 +1,17 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the following HTML file. The file should be created as a separate HTML file and served by the web server.
+
+
+// <!DOCTYPE html>
+// <html>
+// <head>
+//   <title>My Comment</title>
+// </head>
+// <body>
+//   <h1>My Comment</h1>
+//   <p>This is my comment.</p>
+// </body>
+// </html>
+// The server should respond with the above HTML file when a GET request is made to the root URL. The HTML file should be created as a separate file and should not be embedded in the server code.
+
+
